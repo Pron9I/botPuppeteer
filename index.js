2ec13b8219903505c8dd3e74fb13e9c318bb9b8b
Pron9I <vladpron2016y@yandex.ru>
2020-05-03 17:46:06 +0300

    Fixing deployment issue

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ async function ATIparse(cityLoad, radLoad) {
         const puppeteer = require('puppeteer');
         async function parse() {
             try {
-                const browser = await puppeteer.launch();
+                const browser = await puppeteer.launch({ 'args': ['--no-sandbox', '--disable-setuid-sandbox'] });
                 const page = await browser.newPage();
                 await page.goto('https://loads.ati.su/');
                 await page.type('#weightTo', '11', { delay: 0 });
